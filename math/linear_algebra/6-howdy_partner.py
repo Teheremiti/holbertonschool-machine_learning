5d8af542aa9912c3da83ce457ec2260b11fcb452
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-12-04 21:57:47 +0100

    Concatenate lists

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" cat_arrays function """
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two lists
+
+    Args:
+        arr1 (list): List to concatenate into
+        arr2 (list): List to concatenate from
+
+    Returns:
+        list: The concatenanted list
+    """
+    return arr1 + arr2
