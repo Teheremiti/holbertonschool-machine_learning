92ad17e96bec4b1008c06e8473c971649731182b
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-12-05 13:19:04 +0100

    Multiply matrix

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" mat_mul function """
+
+
+def mat_mul(mat1, mat2):
+    """_summary_
+
+    Args:
+        mat1 (2D-list): List to multiply
+        mat2 (2D-list): List to multiply by
+
+    Returns:
+        2D-list: The multiple of mat1 and mat2
+    """
+    mul = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat2[0])):
+            sum = 0
+            for k in range(len(mat2)):
+                sum += mat1[i][k] * mat2[k][j]
+            row.append(sum)
+        mul.append(row)
+    return mul
