0973301e9ffc4aab88ad89aa0bc1bf7ad698f06e
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-12-05 14:37:17 +0100

    ndarray.shape

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" np_shape function """
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy.ndarray
+
+    Args:
+        matrix (numpy.ndarray): Matrix to return the shape of
+
+    Returns:
+        tuple: The shape of the matrix
+    """
+    return matrix.shape
