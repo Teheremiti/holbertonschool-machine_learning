2fd3cd38caf59bcba927131d1905ab2599f576ff
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-12-05 14:40:56 +0100

    Matrix transposition

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" np_transpose function """
+
+
+def np_transpose(matrix):
+    """
+    Transposes the matrix
+
+    Args:
+        matrix (ndarray): The matrix to transpose
+
+    Returns:
+        ndarray: The transposed matrix
+    """
+    return matrix.T
