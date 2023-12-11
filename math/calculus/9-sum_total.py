d00afce2af678ea962301d321406901afd4968bf
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-12-11 12:42:51 +0100

    Sum of squared integers

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" summation_i_squared function """
+
+
+def summation_i_squared(n):
+    """
+    Returns the sum from 1 to n of squared integers
+
+    Args:
+        n (int): The stopping condition
+
+    Returns:
+        int: The total sum
+    """
+    if type(n) is not int:
+        return None
+
+    sum = (n * (n + 1) * (2 * n + 1)) // 6
+    return sum
