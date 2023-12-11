b25912b08518f602eb8e92a9952c8ffa1a79d12c
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-12-11 14:29:12 +0100

    Polynomial integration

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" poly_integral function """
+
+
+def poly_integral(poly, C=0):
+    """
+    Returns the integral of a polynomial
+
+    Args:
+        poly (list): The polynomial as a list of coefficients
+        C (int, optional): The integration constant. Defaults to 0.
+
+    Returns:
+        list: The integral polynomial as a list
+    """
+    if not isinstance(poly, list)\
+       or any([not isinstance(ele, int) for ele in poly])\
+       or not isinstance(C, int):
+        return None
+
+    integral = [C]
+    for power in range(len(poly)):
+        coef = poly[power]
+        if coef % (power + 1) == 0:
+            new_coef = coef // (power + 1)
+        else:
+            new_coef = coef / (power + 1)
+        integral.append(new_coef)
+    return integral
