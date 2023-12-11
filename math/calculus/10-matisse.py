67ec3375a19c08bbda70db3311f40a974f28dd8e
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-12-11 12:58:35 +0100

    Polynomial derivative

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" poly_derivative function """
+
+
+def poly_derivative(poly):
+    """
+    Returns the derivative of a polynomial
+
+    Args:
+        poly (list): The polynomial as a list of coefficients. The index in the
+        list represents the power of x.
+
+    Returns:
+        list: The list of coefficients corresponding to the derivative of poly
+    """
+    if not isinstance(poly, list)\
+       or any([not isinstance(ele, int) for ele in poly]):
+        return None
+
+    derivative = []
+    for power in range(1, len(poly)):
+        coef = poly[power]
+        derivative.append(power * coef)
+    return derivative
