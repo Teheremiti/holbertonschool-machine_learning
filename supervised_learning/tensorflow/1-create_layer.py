7a1fbcf5f90b220ff6cfd52e29bdb2ae7e18dee8
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2024-02-17 13:10:58 +0100

    Layer

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" create _layer function """
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    Creates a layer for the input data.
+
+    Args:
+        prev (Tensor): The tensor output of the previous layer.
+        n (int): The number of nodes in the layer to create.
+        activation (str): The activation function that the layer should use.
+
+    Returns:
+        The tensor output of the layer.
+    """
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(n, activation=activation,
+                                  kernel_initializer=initializer)
+    return layer(prev)
