2b233b47a02eab8882a3152f9202fcc402aa4db1
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2024-03-02 22:09:43 +0100

    Upgrade learning rate decay

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Learning Rate decay upgraded """
+import tensorflow.compat.v1 as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Creates learning rate decay operation in tensorflow using inverse
+    time decay.
+
+    Args:
+        alpha (float): The original learning rate.
+        decay_rate (float): The weight used to determine the rate at which alpha
+            will decay.
+        global_step (int): The number of passes of gradient descent that have
+            elapsed.
+        decay_step (int): The number of passes of gradient descent that should
+            occur before alpha is decayed further.
+
+    Returns:
+        The learning rate decay operation.
+    """
+    return tf.compat.v1.train.inverse_time_decay(
+        learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        global_step=global_step,
+        staircase=True)
