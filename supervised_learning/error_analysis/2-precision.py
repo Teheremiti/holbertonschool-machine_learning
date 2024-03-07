6cc9d197f9778bf2eaeeba89a7e35683d8def6ad
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2024-03-07 13:47:20 +0100

    Precision

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+    Precision
+"""
+
+import numpy as np
+
+
+def precision(confusion):
+    """
+    Calculates the precision for each class in a confusion matrix.
+
+    Args:
+        confusion (ndarray): Matrix of shape (classes, classes) where row
+            indices represent the correct labels and column indices represent
+            the predicted labels.
+
+    Returns:
+        ndarray: Matrix of shape (classes,) containing the precision of each
+            class.
+    """
+    classes = confusion.shape[0]
+    precision = np.zeros((classes,))
+
+    for i in range(classes):
+        true_positif = confusion[i, i]
+        false_positives = np.sum(confusion[:, i]) - true_positif
+        precision[i] = true_positif / (true_positif + false_positives)
+
+    return precision
