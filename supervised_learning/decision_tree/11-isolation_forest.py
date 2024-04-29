0072b76b1f81f97affe37f73102b4497fd2d87c1
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2024-04-29 12:12:07 +0200

    documentation

--- supervised_learning/decision_tree/11-isolation_forest.py
+++ supervised_learning/decision_tree/11-isolation_forest.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python3
+""" Isolation_Random_Forest class """
 
 import numpy as np
 Isolation_Random_Tree = __import__('10-isolation_tree').Isolation_Random_Tree
 
 
 class Isolation_Random_Forest():
+    """
+    Defines the Isolation Random Forest class.
+
+    This class provides methods for training a forest of Isolation Random
+    Trees, making predictions, and identifying suspicious instances based on
+    mean depth.
+
+    Attributes:
+        numpy_predicts (list): List of functions for predicting with each tree.
+        target (None): Placeholder for the target variable.
+        numpy_preds (None): Placeholder for the predictions.
+        n_trees (int): Number of trees in the forest.
+        max_depth (int): Maximum depth of each tree.
+        seed (int): Seed for the random number generator.
+    """
+
     def __init__(self, n_trees=100, max_depth=10, min_pop=1, seed=0):
+        """
+        Initialize the Isolation Random Forest model.
+
+        Args:
+            n_trees (int, optional): Number of trees in the forest.
+                Defaults to 100.
+            max_depth (int, optional): Maximum depth of each tree.
+                Defaults to 10.
+            min_pop (int, optional): Minimum population of a node to not become
+                a leaf. Defaults to 1.
+            seed (int, optional): Seed for the random number generator.
+                Defaults to 0.
+        """
         self.numpy_predicts = []
         self.target = None
         self.numpy_preds = None
@@ -14,10 +44,27 @@ class Isolation_Random_Forest():
         self.seed = seed
 
     def predict(self, explanatory):
+        """
+        Make predictions using the Isolation Random Forest model.
+
+        Args:
+            explanatory (numpy.ndarray): Explanatory features for prediction.
+
+        Returns:
+            numpy.ndarray: Predicted values.
+        """
         predictions = np.array([f(explanatory) for f in self.numpy_preds])
         return predictions.mean(axis=0)
 
     def fit(self, explanatory, n_trees=100, verbose=0):
+        """
+        Fit the Isolation Random Forest model to the training data.
+
+        Args:
+            explanatory (numpy.ndarray): Explanatory features for training.
+            n_trees (int, optional): Number of trees to train. Defaults to 100.
+            verbose (int, optional): Verbosity level. Defaults to 0.
+        """
         self.explanatory = explanatory
         self.numpy_preds = []
         depths = []
