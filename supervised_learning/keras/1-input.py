0e7b2a6aedeef45a595fd2214f920b768086ca6d
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2024-05-06 16:52:49 +0200

    Build model with Input

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python3
-"""
-    Input
-"""
+""" Input """
 
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-        function that builds a neural network with the Keras library
+    Builds a neural network with the Keras library.
 
-        :param nx: number of input features to the network
-        :param layers: list, number nodes in each layer
-        :param activations: list, activation functions for each layer
-        :param lambtha: L2 regularization parameter
-        :param keep_prob: proba node kept for dropout
+    Args:
+        nx (int): The number of input features to the network.
+        layers (list): Contains the number of nodes in each layer of the
+            network.
+        activations (list): Contains the activation functions used for each
+            layer of the network.
+        lambtha (float): The L2 regularization parameter.
+        keep_prob (float): Probability that a node will be kept for dropout.
 
-        :return: keras model
+    Returns:
+        The Keras model.
     """
     inputs = K.Input(shape=(nx,))
-
     x = inputs
     for i in range(len(layers)):
-        # add Dense layer
         x = K.layers.Dense(layers[i],
                            activation=activations[i],
                            kernel_regularizer=K.regularizers.L2(lambtha))(x)
 
-        # apply Dropout except last layer
         if i != len(layers) - 1 and keep_prob is not None:
             x = K.layers.Dropout(1 - keep_prob)(x)
-
-    # create model
     model = K.Model(inputs, x)
-
     return model
