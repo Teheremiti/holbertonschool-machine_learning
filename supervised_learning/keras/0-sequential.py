a20507b0a30c1be1c8a0f09aca73607169653c7d
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2024-05-06 16:28:39 +0200

    Build model with Keras

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
-"""
-    Sequential
-"""
 
-import tensorflow.keras as K
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Dropout
+from tensorflow.python.keras.regularizers import L2
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-        function that builds a neural network with the Keras library
+    Builds a neural network with the Keras library.
 
-        :param nx: number of input features to the network
-        :param layers: list, number nodes in each layer
-        :param activations: list, activation functions for each layer
-        :param lambtha: L2 regularization parameter
-        :param keep_prob: proba node kept for dropout
+    Args:
+        nx (int): The number of input features to the network.
+        layers (list): Contains the number of nodes in each layer of the
+            network.
+        activations (list): Contains the activation functions used for each
+            layer of the network.
+        lambtha (float): The L2 regularization parameter.
+        keep_prob (float): The probability that a node will be kept for dropout.
 
-        :return: keras model
+    Returns:
+        The Keras model.
     """
-    model = K.Sequential()
-
+    model = Sequential()
     for i in range(len(layers)):
-        model.add(K.layers.Dense(layers[i],
-                  activation=activations[i],
-                  kernel_regularizer=K.regularizers.L2(lambtha),
-                  input_dim=nx))
-        # apply dropout except on output layer
-        if i != len(layers) - 1 and keep_prob is not None:
-            model.add(K.layers.Dropout(1-keep_prob))
+        model.add(Dense(units=layers[i],
+                        activation=activations[i],
+                        kernel_regularizer=L2(lambtha),
+                        input_dim=nx))
 
+        if i != len(layers) - 1 and keep_prob is not None:
+            model.add(Dropout(rate=1 - keep_prob))
     return model
