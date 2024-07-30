75d50eaebbf8f57520ff4d9da25012260254ebea
Teheremiti <t.tuiaiho@gmail.com>
2024-07-30 13:38:50 +0200

    Stacking bars

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
+""" Stacking Bars """
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4, 3))
 
-names = ['Farrah', 'Fred', 'Felicia']
-colors = ['r', 'yellow', '#ff8000', '#ffe5b4']
-labels = ['apples', 'bananas', 'oranges', 'peaches']
+def bars():
+    """ Plots a stacked bar graph. """
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
 
-bottom = 0
-for i in range(len(fruit)):
-    plt.bar(
-        names,
-        fruit[i],
-        width=0.5,
-        bottom=bottom,
-        color=colors[i],
-        label=labels[i]
-        )
-    bottom += fruit[i]
+    names = ['Farrah', 'Fred', 'Felicia']
+    colors = ['r', 'yellow', '#ff8000', '#ffe5b4']
+    labels = ['apples', 'bananas', 'oranges', 'peaches']
 
-plt.title('Number of Fruit per Person')
-plt.ylabel('Quantity of Fruit')
-plt.yticks(range(0, 81, 10))
-plt.legend()
+    bottom = 0
+    for i in range(len(fruit)):
+        plt.bar(
+            names,
+            fruit[i],
+            width=0.5,
+            bottom=bottom,
+            color=colors[i],
+            label=labels[i]
+            )
+        bottom += fruit[i]
 
-plt.show()
+    plt.title('Number of Fruit per Person')
+    plt.ylabel('Quantity of Fruit')
+    plt.yticks(range(0, 81, 10))
+    plt.legend()
+
+    plt.show()
