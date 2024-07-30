7a825014d0664fa59d3c9f7fafbc5089bccc887b
Teheremiti <t.tuiaiho@gmail.com>
2024-07-30 11:32:19 +0200

    Line graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,8 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
 
-plt.plot(y, 'r')
-plt.xlim(0, 10)
-plt.show()
+def line():
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(y, 'r')
+    plt.xlim(0, 10)
+    plt.show()
