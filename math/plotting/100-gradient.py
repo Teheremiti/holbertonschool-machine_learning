ccfb84373e342861bd636bd4a2a5ab681b6f71c3
Teheremiti <t.tuiaiho@gmail.com>
2024-07-30 13:40:56 +0200

    Gradient

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
+""" Gradient """
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
 
-x = np.random.randn(2000) * 10
-y = np.random.randn(2000) * 10
-z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+def gradient():
+    """ Scatter plots sampled elevations on a mountain. """
+    np.random.seed(5)
 
-plt.title('Mountain Elevation')
-plt.scatter(x, y, c=z)
-plt.xlabel('x coordinate (m)')
-plt.ylabel('y coordinate (m)')
-plt.colorbar().set_label('elevation (m)')
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-plt.show()
+    plt.title('Mountain Elevation')
+    plt.scatter(x, y, c=z)
+    plt.xlabel('x coordinate (m)')
+    plt.ylabel('y coordinate (m)')
+    plt.colorbar().set_label('elevation (m)')
+
+    plt.show()
