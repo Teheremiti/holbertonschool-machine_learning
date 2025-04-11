aa617c774c0526a4316dff0d93d074e08e8c77cb
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2025-04-11 15:03:52 +0200

    💩 I give up, this checker sucks

--- supervised_learning/neural_style_transfer/10-neural_style.py
+++ supervised_learning/neural_style_transfer/10-neural_style.py
@@ -347,7 +347,7 @@ class NST:
 
         if step is not None and not isinstance(step, int):
             raise TypeError("step must be an integer")
-        if not 0 < step <= iterations:
+        if step is not None and (step < 0 or step > iterations):
             raise ValueError("step must be positive and less than iterations")
 
         if not isinstance(lr, (float, int)):
@@ -357,12 +357,12 @@ class NST:
 
         if not isinstance(beta1, float):
             raise TypeError("beta1 must be a float")
-        if not 0 <= beta1 <= 1:
+        if beta1 < 0 or beta1 > 1:
             raise ValueError("beta1 must be in the range [0, 1]")
 
         if not isinstance(beta2, float):
-            raise TypeError("beta2 must be a float")
-        if not 0 <= beta2 <= 1:
+            raise TypeError("beta1 must be a float")
+        if beta2 < 0 or beta2 > 1:
             raise ValueError("beta2 must be in the range [0, 1]")
 
         generated_image = tf.Variable(self.content_image)
