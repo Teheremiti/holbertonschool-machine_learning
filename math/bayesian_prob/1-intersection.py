35878b04ad36a8650071ab712905c19ff4acdc64
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2025-05-20 15:33:34 +0200

    Intersection

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Intersection"""
+import numpy as np
+
+likelihood = __import__('0-likelihood').likelihood
+
+
+def intersection(x, n, P, Pr):
+    """
+    Calculates the intersection of obtaining this data with the various
+    hypothetical probabilities.
+
+    Args:
+    x (int): Number of patients that develop severe side effects.
+    n (int): Total number of patients observed.
+    P (np.ndarray): 1D arraycontaining the various hypothetical probabilities
+    of developing severe side effects.
+    Pr (np.ndarray): 1D array containing the prior beliefs of P.
+
+    Returns:
+    np.ndarray: The 1D array containing the intersection of obtaining x and n
+    with each probability P, respectively.
+    """
+
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    likelihoods = likelihood(x, n, P)
+    intersections = likelihoods * Pr
+    return intersections
