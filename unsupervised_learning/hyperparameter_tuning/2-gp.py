7e85998d27ebe89a94c1b546967bd32b24ea3ab2
Teheremiti <t.tuiaiho@gmail.com>
2025-08-07 22:54:06 +0200

    Update Gaussian Process

---
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+GaussianProcess class for 1D Gaussian Process Regression.
+"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """
+    Represents a noiseless 1D Gaussian Process.
+    """
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Initializes the Gaussian Process.
+
+        Args:
+            X_init (np.ndarray): shape (t, 1), the inputs sampled
+            Y_init (np.ndarray): shape (t, 1), the outputs of
+                the black-box function
+            l (float): length-scale parameter
+            sigma_f (float): signal variance
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix between two inputs using
+        the RBF kernel.
+
+        Args:
+            X1 (np.ndarray): shape (m, 1)
+            X2 (np.ndarray): shape (n, 1)
+
+        Returns:
+            Covariance matrix as np.ndarray of shape (m, n)
+        """
+        sqdist = np.sum(X1**2, axis=1).reshape(-1, 1) + \
+            np.sum(X2**2, axis=1) - 2 * np.dot(X1, X2.T)
+        return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
+
+    def predict(self, X_s):
+        """
+        Predicts the mean and variance of the Gaussian Process at new points.
+
+        Args:
+            X_s (np.ndarray): shape (s, 1), the points to predict
+
+        Returns:
+            mu (np.ndarray): shape (s,), predictive mean
+            sigma (np.ndarray): shape (s,), predictive variance
+        """
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(self.K)
+
+        mu_s = K_s.T.dot(K_inv).dot(self.Y).reshape(-1)
+        cov_s = K_ss - K_s.T.dot(K_inv).dot(K_s)
+        sigma_s = np.diag(cov_s)
+
+        return mu_s, sigma_s
+
+    def update(self, X_new, Y_new):
+        """
+        Updates the Gaussian Process with a new sample.
+
+        Args:
+            X_new (np.ndarray): shape (1,), new sample input
+            Y_new (np.ndarray): shape (1,), new sample output
+
+        Modifies:
+            self.X, self.Y, self.K
+        """
+        X_new = X_new.reshape(1, 1)
+        Y_new = Y_new.reshape(1, 1)
+
+        self.X = np.vstack((self.X, X_new))
+        self.Y = np.vstack((self.Y, Y_new))
+        self.K = self.kernel(self.X, self.X)
