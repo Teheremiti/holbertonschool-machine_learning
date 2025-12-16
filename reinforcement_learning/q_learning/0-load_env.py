f2403ef09e2fef00b643f04b5c7e4452009db166
Teheremiti <t.tuiaiho@gmail.com>
2025-12-16 11:43:18 +0100

    Fix environment name

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -23,7 +23,7 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
         The created FrozenLake environment instance.
     """
     env = gym.make(
-        "FrozenLake-v0",
+        "FrozenLake-v1",
         desc=desc,
         map_name=map_name,
         is_slippery=is_slippery,
