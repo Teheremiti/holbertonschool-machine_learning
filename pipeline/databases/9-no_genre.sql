1af7132c3fc93f539aed3da734ed4040db356f45
Teheremiti <t.tuiaiho@gmail.com>
2026-03-25 11:22:34 +0100

    Fix syntax error

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,7 +1,7 @@
 -- Shows with no genres
-SELECT show.title, show_genre.genre_id
-FROM tv_shows AS show
-LEFT JOIN tv_show_genres AS show_genre
-ON show.id = show_genre.show_id
-WHERE show_genre.genre_id IS NULL
-ORDER BY show.title ASC, show_genre.genre_id ASC;
+SELECT shows.title, genres.genre_id
+FROM tv_shows AS shows
+LEFT JOIN tv_show_genres AS genres
+ON shows.id = show_genre.show_id
+WHERE genres.genre_id IS NULL
+ORDER BY shows.title ASC, genres.genre_id ASC;
