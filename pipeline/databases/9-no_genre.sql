64fd64cb2e52487a02f5337f846b7f60280da1b9
Teheremiti <t.tuiaiho@gmail.com>
2026-03-25 11:24:15 +0100

    Fix typo on column name

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -2,6 +2,6 @@
 SELECT shows.title, genres.genre_id
 FROM tv_shows AS shows
 LEFT JOIN tv_show_genres AS genres
-ON shows.id = show_genre.show_id
+ON shows.id = genres.show_id
 WHERE genres.genre_id IS NULL
 ORDER BY shows.title ASC, genres.genre_id ASC;
