0efbe18bc49fb9bfff28e8f4030affbef70dba2b
Teheremiti <t.tuiaiho@gmail.com>
2026-03-27 10:02:08 +0100

    Safe division

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,15 @@
+-- Safe division function
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(
+    a INT,
+    b INT)
+    RETURNS FLOAT
+
+    BEGIN
+        RETURN CASE WHEN b = 0 THEN 0 ELSE a / b END;
+    END $$
+
+DELIMITER;
