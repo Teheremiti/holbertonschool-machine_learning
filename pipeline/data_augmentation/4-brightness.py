f0a274a295bcceca3c3c3c601a92de37d1f78077
Teheremiti <t.tuiaiho@gmail.com>
2026-03-27 20:56:30 +0100

    Change brightness

---
+++ pipeline/data_augmentation/4-brightness.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Adjust image brightness."""
+import tensorflow as tf
+
+
+def change_brightness(image, max_delta):
+    """Change an image's brightness.
+
+    Args:
+        image: A `tf.Tensor` representing the image.
+        max_delta: Brightness delta applied to the image.
+
+    Returns:
+        A `tf.Tensor` containing the brightness-adjusted image.
+    """
+    return tf.image.adjust_brightness(image, max_delta)
